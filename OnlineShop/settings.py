d67626eb3316e0ea7dde242370272c9ab87cb7fb
mirafzal114 <mirafzaldevelop@gmail.com>
2023-08-30 17:45:47 +0500

    pagination qoshildi

--- OnlineShop/settings.py
+++ OnlineShop/settings.py
@@ -126,5 +126,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
-    'PAGE_SIZE': 5
+    'PAGE_SIZE': 4,
+
 }
--- product/views.py
+++ product/views.py
@@ -3,17 +3,25 @@ from rest_framework import generics
 from .serializer import ProductSerializer,  ProductTypeSerializer
 from .models import Product, ProductType
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.pagination import PageNumberPagination
+
+# Paginationni  ozimzga qulay xolga keltirip oldik
+class ProductAPIListPagination(PageNumberPagination):
+    page_size = 4
+    page_size_query_param = 'page_size'
+    max_page_size = 12
 
 class ProducTypeList(generics.ListCreateAPIView):
     queryset = ProductType.objects.all()
     serializer_class = ProductTypeSerializer
     permission_classes = (IsAuthenticated,)
+    pagination_class = ProductAPIListPagination
 
 class ProductTypeDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = ProductType.objects.all()
     serializer_class = ProductTypeSerializer
     permission_classes = (IsAuthenticated,)
-    permission_classes = (IsAuthenticated,)
+
 
 class ProductList(generics.ListCreateAPIView):
     queryset = Product.objects.all()
